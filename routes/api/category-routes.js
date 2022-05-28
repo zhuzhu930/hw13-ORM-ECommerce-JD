f2ad3ede6a864481acf23fdc408cba912e1082f9
Jessie Doherty <zhuzhu930@yahoo.com>
2022-05-27 22:49:45 -0500

    writing category-routes.js

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -41,17 +41,47 @@ router.post('/', async (req, res) => {
   // create a new category
   try {
     const categoryData = await Category.create({
-      //stopped here.
-    })
+      category_name: req.body.category_name,
+    });
+    res.status(200).json(categoryData)
+  } catch (err) {
+    res.status(400).json(err);
   }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a category by its `id` value
+  try {
+    const category = await Category.update(
+      {category_name: req.body.category_name},
+      {
+        where: {
+          id: req.params.id,
+        },
+      });
+      res.status(200).json(category);
+  } catch (err) {
+    res.status(500).json(err);
+  };
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
+  try {
+    const categoryData = await Category.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+
+    if(!categoryData) {
+      res.status(404).json({ message: 'No category found with this id!'});
+      return;
+    }
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
